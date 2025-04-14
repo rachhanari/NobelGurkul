55bc57a9c04acfea423ac4d410e101410638ec38
rachhanari <racchanarendra123@gmail.com>
2025-04-15 03:14:45 +0530

    vercel change

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/NobelGurkul/', // 👈 This is essential for GitHub Pages or any subdirectory deployment
-
   plugins: [react()],
-
   server: {
     port: 5173,
     open: true,
   },
-
   build: {
-    outDir: 'dist',       // Output folder for production build
-    sourcemap: true,      // Helpful for debugging
-    emptyOutDir: true,    // Clean old build files before building again
+    outDir: 'dist',
+    sourcemap: true,
   },
 });
